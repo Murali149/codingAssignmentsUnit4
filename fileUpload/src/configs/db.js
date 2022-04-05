3697ac08e754551c6a5f4e71dbda4e989cdd7865
Murali149 <93724183+Murali149@users.noreply.github.com>
2022-04-05 15:38:20 +0530

    second commit

---
+++ fileUpload/src/configs/db.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose")
+
+module.exports = ()=>{
+    // return mongoose.connect("mongodb+srv://dbTest:<password>@cluster0.mdvv2.mongodb.net/register?retryWrites=true&w=majority")
+}
